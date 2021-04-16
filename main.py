3f23a6366cff3ae11b25b75f49d81e080a9ce86a
paulcull <paul@pkhome.co.uk>
2021-04-16 12:33:28 +0100

    udpates

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-def df_dict(df):
-    dict = {}
+# function to expand all columns in a frame with a substitution dictionary
+def df_expand(df, di):
+    notfirstcol = False
     for col in df:
-        dict[col] = df[col].unique().tolist() # unique method optional
-    return dict
+        print(col)
+        print(notfirstcol)
+        if  notfirstcol:
+            df[col].replace(di, inplace=True)
+        notfirstcol = True
+    return df
+
+def df_collapse(df):
+    df['Expanded'] = df[df.columns[1:]].apply(lambda x: ' '.join(x.dropna().astype(str)),axis=1)
+    return df
 
 
 df_we = pd.read_excel('./data/word_compressions.xlsx')
@@ -12,35 +21,27 @@ df_we = pd.read_excel('./data/word_compressions.xlsx')
 di_we = pd.Series(df_we.Expand.values,index=df_we.CODE).to_dict()
 
 df_dd = pd.read_excel('./data/data_dictionary.xlsx')
-# df_dm = pd.read_excel('./input/datamodel.xlsx',converters={'TABLE':di_we, 'Attribute':di_we})
 df_dm = pd.read_excel('./input/datamodel.xlsx')
 print(df_dm)
 
 print("start with the data model...")
 
-# for index, row in df_dm.iterrows():
-#     print(row['Table'] , "." , row['Attribute'])
-#     row['Table'].replace()
+df_dm_table = pd.concat([df_dm['Table'], df_dm['Table'].str.split('_', expand=True)], axis=1)
+df_dm_attribute = pd.concat([df_dm['Attribute'], df_dm['Attribute'].str.split('_', expand=True)], axis=1)
+print(df_dm_table)
+print(df_dm_attribute)
 
-# df_we.set_index('CODE')
-# df_dm = df_dm.set_index('TABLE').rename( columns = {'TABLE':'Expand'})
+df_expand(df_dm_table,di_we)
+df_expand(df_dm_attribute,di_we)
 
-# df_we.columns=['Table','Word']
-# print(df_we)
-# df_exp_model = pd.merge(
-# df_dm,df_we, how='left', on=["Table"]
-# )
-# df_we.columns=['Attribute','Word2']
-# df_exp_model2 = pd.merge(
-# df_exp_model,df_we, how='left', on=["Attribute"]
-# )
+print(df_dm_table)
+print(df_dm_attribute)
 
-# print(df_exp_model2)
+df_collapse(df_dm_table)
+df_collapse(df_dm_attribute)
 
-# df_dm['Table'].map(di_we).fillna(df_dm['Table'])
-df_dm['Table'].replace(di_we, inplace=True)
-df_dm['Attribute'].replace(di_we, inplace=True)
-print(df_dm)
+print(df_dm_table)
+print(df_dm_attribute)
 
 
 
